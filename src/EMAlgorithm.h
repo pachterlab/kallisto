784068258b5c05e42050587edb62f208f5b37698
Harold Pimentel <haroldpimentel@gmail.com>
2015-02-04 20:35:27 -0800

    implement rho. getting inf rho

--- src/EMAlgorithm.h
+++ src/EMAlgorithm.h
@@ -8,6 +8,9 @@
 #include <iostream>
 #include <vector>
 
+
+const double TOLERANCE = 1e-5;;
+
 template <typename Index>
 struct EMAlgorithm {
 
@@ -16,41 +19,57 @@ struct EMAlgorithm {
     // TODO: refactor to remove dependence on Index
 	EMAlgorithm(const ProgramOptions& opt, const Index& idx,
 	        const std::vector<int>& counts,
-	        const std::vector<double>& eff_lens) :
+	        const std::vector<double>& eff_lens,
+	        const WeightMap& wm) :
 	    idx_(idx),
 	    counts_(counts),
 	    num_trans_(idx.num_trans),
 	    eff_lens_(eff_lens),
-	    alpha_(idx.num_trans, 1.0/idx.num_trans) // uniform distribution over transcripts
+	    weight_map_(wm),
+	    alpha_(idx.num_trans, 1.0/idx.num_trans), // uniform distribution over transcripts
+	    rho_(idx.num_trans, 0.0),
+	    rho_set_(false)
 	{}
 
 	~EMAlgorithm() {}
 
-	void run(const WeightMap& weight_map, size_t n_iter = 500) {
+	void run(size_t n_iter = 500) {
         std::vector<double> next_alpha(alpha_.size(), 0.0);
 
-        assert(weight_map.size() == counts_.size());
+        assert(weight_map_.size() == counts_.size());
 
         // // there is a denominator normalizer for every single equivalence
         // // class
         // std::vector<double> denom(counts_.size(), 0.0);
 
+        double denom;
 	    for (auto i = 0; i < n_iter; ++i) {
 
+            std::cout << "it: " << i << std::endl;
             for (auto& ec_kv : idx_.ecmap ) {
 
-                double denom {0.0};
+                denom = 0.0;
                 // first, compute the denominator: a normalizer
                 // iterate over transcripts in EC map
-                auto w_search = weight_map.find(ec_kv.first);
+                auto w_search = weight_map_.find(ec_kv.first);
 
                 // everything in ecmap should be in weight_map
-                assert( w_search != weight_map.end() );
+                assert( w_search != weight_map_.end() );
                 assert( w_search->second.size() == ec_kv.second.size() );
+
+                /* std::cout << "denom: " << denom << "\t"; */
 
                 for (auto t_it = 0; t_it < ec_kv.second.size(); ++t_it) {
                     denom += ec_kv.second[t_it] * w_search->second[t_it];
                 }
+
+                if (denom < TOLERANCE) {
+                    continue;
+                }
+
+                std::cout << "denom: " << denom << std::endl;
+
+                // TODO: what happens if denom == 0.0? (or close to?)
 
                 // next, compute the update step
 
@@ -68,11 +87,61 @@ struct EMAlgorithm {
 	    }
 	}
 
+    void compute_rho() {
+
+        if (rho_set_) {
+            // rho has already been set, let's update it
+            std::fill(rho_.begin(), rho_.end(), 0.0);
+        }
+
+        for (auto& ec_kv : idx_.ecmap ) {
+
+            double denom {0.0};
+            // first, compute the denominator: a normalizer
+            // iterate over transcripts in EC map
+            auto w_search = weight_map_.find(ec_kv.first);
+
+            // everything in ecmap should be in weight_map
+            assert( w_search != weight_map_.end() );
+            assert( w_search->second.size() == ec_kv.second.size() );
+
+            for (auto t_it = 0; t_it < ec_kv.second.size(); ++t_it) {
+                denom += ec_kv.second[t_it] * w_search->second[t_it];
+            }
+
+            if (denom < TOLERANCE) {
+                continue;
+            }
+            // next, compute the update step
+
+            for (auto t_it = 0; t_it < ec_kv.second.size(); ++t_it) {
+                rho_[t_it] += alpha_[t_it] * w_search->second[t_it] / denom;
+            }
+        }
+
+        rho_set_ = true;
+    }
+
+    void write(const std::string& rho_out) const {
+        std::ofstream out;
+        out.open(rho_out, std::ios::out);
+
+        for (auto i = 0; i < rho_.size(); ++i) {
+            out << i << "\t" << rho_[i] << std::endl;
+            std::cout << alpha_[i] << std::endl;
+        }
+
+        out.close();
+    }
+
 	int num_trans_;
 	const Index &idx_;
 	const std::vector<int>& counts_;
 	const std::vector<double>& eff_lens_;
+	const WeightMap& weight_map_;
     std::vector<double> alpha_;
+    std::vector<double> rho_;
+    bool rho_set_;
 };
 
 #endif // KALLISTO_EMALGORITHM_H
