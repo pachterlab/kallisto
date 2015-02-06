1a65a6bcadcbfa5c8d3d1671a2f8e48a2c3c4fb5
Harold Pimentel <haroldpimentel@gmail.com>
2015-02-06 15:23:23 -0800

    load data bit more cleanly

--- benchmark/load_data.R
+++ benchmark/load_data.R
@@ -28,52 +28,55 @@ jsd <- function(p, q, thresh=10^-16) {
 }
 
 read_sailfish <- function(fname) {
-    sf <- as.data.frame(fread(fname, header = FALSE, skip = 5),
-        stringsAsFactors = FALSE)
+    sf <- fread(fname, header = FALSE, skip = 5, stringsAsFactors = FALSE,
+        data.table = FALSE)
     colnames(sf) <- c("target_id", "length", "tpm", "rpkm", "kpkm", "est_counts")
     sf %>%
         arrange(target_id)
 }
 
 read_kallisto <- function(fname) {
-    kal <- as.data.frame(fread(fname, header = TRUE), stringsAsFactors = FALSE)
+    kal <- fread(fname, header = TRUE, stringsAsFactors = FALSE,
+        data.table = FALSE)
     kal %>%
         arrange(target_id)
 }
 
 read_kallisto_py <- function(fname) {
-    kal <- as.data.frame(fread(fname, header = TRUE), stringsAsFactors = FALSE)
+    kal <- fread(fname, header = TRUE, stringsAsFactors = FALSE,
+        data.table = FALSE)
     kal %>%
         rename(target_id = name) %>%
         arrange(target_id)
 }
 
 read_salmon <- function(fname) {
-    salmon <- as.data.frame(fread(fname, header = FALSE, skip = 13),
-        stringsAsFactors = FALSE)
+    salmon <- fread(fname, header = FALSE, skip = 13, stringsAsFactors = FALSE,
+        data.table = FALSE)
     colnames(salmon) <- c("target_id", "length", "tpm", "fpkm", "est_counts")
     salmon %>%
         arrange(target_id)
 }
 
 read_xprs <- function(fname) {
-    xprs <- as.data.frame(fread(fname, header = TRUE), stringsAsFactors = FALSE)
+    xprs <- fread(fname, header = TRUE, stringsAsFactors = FALSE,
+        data.table = FALSE)
     xprs %>%
         arrange(target_id)
 }
 
 read_oracle <- function(fname, targ_to_eff_len) {
-    # TODO: figure out how to get data.table::fread to read this file
-    oracle <- read.table(fname, header = FALSE, stringsAsFactors = FALSE)
+    oracle <- fread(paste0("sed 's/^ *//g' ", fname), header = FALSE,
+        data.table = FALSE)
 
     targ_to_eff_len <- select(targ_to_eff_len, target_id, eff_length)
 
-    # bug somewhere in here computing TPM... seems like something is NaN then
-    # screws up normlization?
     oracle %>%
-        rename(counts = V1, target_id = V2) %>%
-        left_join(targ_to_eff_len, by = "target_id") %>%
+        rename(target_id = V1, counts = V2) %>%
+        left_join(targ_to_eff_len, ., by = "target_id") %>%
+        mutate(counts = replace(counts, is.na(counts), 0.0)) %>%
         mutate(tpm = counts / eff_length) %>%
+        mutate(tpm = replace(tpm, eff_length == 0, 0.0)) %>%
         mutate(tpm = 1e6 * tpm / sum(tpm)) %>%
         arrange(target_id)
 }
@@ -82,17 +85,6 @@ sf <- read_sailfish(paste0(base_dir, "/sailfish/quant.sf"))
 kal_py <- read_kallisto_py(paste0(base_dir, "/kal_py/results.kal"))
 salmon <- read_salmon(paste0(base_dir, "/salmon/quant.sf"))
 xprs <- read_xprs(paste0(base_dir, "/express/results.xprs"))
-
-# TPM isn't being computed correctly
 oracle <- read_oracle(paste0(base_dir, "/oracle.counts"), xprs)
 
-# TODO: read this
-# temp = read.table(sprintf("%s/non_ase_levels.xprs", base_dir), row.names=1, header=FALSE)
-# truth = data.frame(tpm=rep(0, nrow(sf)), row.names=rownames(sf))
-# truth[rownames(temp),] = temp
-
-# temp = read.table(sprintf("%s/oracle.counts", base_dir), row.names=2, header=FALSE)
-# oracle = data.frame(tpm=rep(0, nrow(sf)), counts=rep(0, nrow(sf)), row.names=rownames(sf))
-# oracle[rownames(temp), "counts"] = temp
-# oracle[, "tpm"] = oracle[,"counts"]/xprs[,"eff_length"]
-# oracle[, "tpm"] = 10^6*oracle[, "tpm"]/sum(oracle[, "tpm"])
+# fread(paste0(base_dir, "/non_ase_levels.xprs"), header = FALSE, data.table = FALSE)
