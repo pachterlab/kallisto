b5da0ad38c057c95a8ed094fd2a02c2ca86711f2
Pall Melsted <pmelsted@gmail.com>
2015-02-06 10:28:13 -0800

    Reformatting using astyle

--- src/hash.cpp
+++ src/hash.cpp
@@ -6,49 +6,49 @@ uint64_t inline _rotl64(uint64_t value, int8_t amount) {
   return ((value) << (amount)) | ((value) >> (64 - (amount)));
 }
 
-uint32_t SuperFastHash (const char * data, int len) {
-uint32_t hash = len, tmp;
-int rem;
+uint32_t SuperFastHash (const char *data, int len) {
+  uint32_t hash = len, tmp;
+  int rem;
 
-    if (len <= 0 || data == NULL) return 0;
+  if (len <= 0 || data == NULL) { return 0; }
 
-    rem = len & 3;
-    len >>= 2;
+  rem = len & 3;
+  len >>= 2;
 
-    /* Main loop */
-    for (;len > 0; len--) {
-        hash  += get16bits (data);
-        tmp    = (get16bits (data+2) << 11) ^ hash;
-        hash   = (hash << 16) ^ tmp;
-        data  += 2*sizeof (uint16_t);
-        hash  += hash >> 11;
-    }
+  /* Main loop */
+  for (; len > 0; len--) {
+    hash  += get16bits (data);
+    tmp    = (get16bits (data+2) << 11) ^ hash;
+    hash   = (hash << 16) ^ tmp;
+    data  += 2*sizeof (uint16_t);
+    hash  += hash >> 11;
+  }
 
-    /* Handle end cases */
-    switch (rem) {
-        case 3: hash += get16bits (data);
-                hash ^= hash << 16;
-                hash ^= data[sizeof (uint16_t)] << 18;
-                hash += hash >> 11;
-                break;
-        case 2: hash += get16bits (data);
-                hash ^= hash << 11;
-                hash += hash >> 17;
-                break;
-        case 1: hash += *data;
-                hash ^= hash << 10;
-                hash += hash >> 1;
-    }
-
-    /* Force "avalanching" of final 127 bits */
-    hash ^= hash << 3;
-    hash += hash >> 5;
-    hash ^= hash << 4;
+  /* Handle end cases */
+  switch (rem) {
+  case 3: hash += get16bits (data);
+    hash ^= hash << 16;
+    hash ^= data[sizeof (uint16_t)] << 18;
+    hash += hash >> 11;
+    break;
+  case 2: hash += get16bits (data);
+    hash ^= hash << 11;
     hash += hash >> 17;
-    hash ^= hash << 25;
-    hash += hash >> 6;
+    break;
+  case 1: hash += *data;
+    hash ^= hash << 10;
+    hash += hash >> 1;
+  }
 
-    return hash;
+  /* Force "avalanching" of final 127 bits */
+  hash ^= hash << 3;
+  hash += hash >> 5;
+  hash ^= hash << 4;
+  hash += hash >> 17;
+  hash ^= hash << 25;
+  hash += hash >> 6;
+
+  return hash;
 }
 
 
@@ -58,25 +58,23 @@ int rem;
 // Block read - if your platform needs to do endian-swapping or can only
 // handle aligned reads, do the conversion here
 
-inline uint64_t getblock ( const uint64_t * p, int i )
-{
+inline uint64_t getblock ( const uint64_t *p, int i ) {
   return p[i];
 }
 
 //----------
 // Block mix - combine the key bits with the hash bits and scramble everything
 
-inline void bmix64 ( uint64_t & h1, uint64_t & h2, uint64_t & k1, uint64_t & k2, uint64_t & c1, uint64_t & c2 )
-{
-  k1 *= c1; 
-  k1  = _rotl64(k1,23); 
+inline void bmix64 ( uint64_t& h1, uint64_t& h2, uint64_t& k1, uint64_t& k2, uint64_t& c1, uint64_t& c2 ) {
+  k1 *= c1;
+  k1  = _rotl64(k1,23);
   k1 *= c2;
   h1 ^= k1;
   h1 += h2;
 
   h2 = _rotl64(h2,41);
 
-  k2 *= c2; 
+  k2 *= c2;
   k2  = _rotl64(k2,23);
   k2 *= c1;
   h2 ^= k2;
@@ -92,8 +90,7 @@ inline void bmix64 ( uint64_t & h1, uint64_t & h2, uint64_t & k1, uint64_t & k2,
 //----------
 // Finalization mix - avalanches all bits to within 0.05% bias
 
-inline uint64_t fmix64 ( uint64_t k )
-{
+inline uint64_t fmix64 ( uint64_t k ) {
   k ^= k >> 33;
   k *= 0xff51afd7ed558ccd;
   k ^= k >> 33;
@@ -103,9 +100,8 @@ inline uint64_t fmix64 ( uint64_t k )
   return k;
 }
 
-void MurmurHash3_x64_128 ( const void * key, const int len, const uint32_t seed, void * out )
-{
-  const uint8_t * data = (const uint8_t*)key;
+void MurmurHash3_x64_128 ( const void *key, const int len, const uint32_t seed, void *out ) {
+  const uint8_t *data = (const uint8_t *)key;
   const int nblocks = len / 16;
 
   uint64_t h1 = 0x9368e53c2f6af274 ^ seed;
@@ -117,44 +113,42 @@ void MurmurHash3_x64_128 ( const void * key, const int len, const uint32_t seed,
   //----------
   // body
 
-  const uint64_t * blocks = (const uint64_t *)(data);
+  const uint64_t *blocks = (const uint64_t *)(data);
 
-  for(int i = 0; i < nblocks; i++)
-    {
-      uint64_t k1 = getblock(blocks,i*2+0);
-      uint64_t k2 = getblock(blocks,i*2+1);
+  for(int i = 0; i < nblocks; i++) {
+    uint64_t k1 = getblock(blocks,i*2+0);
+    uint64_t k2 = getblock(blocks,i*2+1);
 
-      bmix64(h1,h2,k1,k2,c1,c2);
-    }
+    bmix64(h1,h2,k1,k2,c1,c2);
+  }
 
   //----------
   // tail
 
-  const uint8_t * tail = (const uint8_t*)(data + nblocks*16);
+  const uint8_t *tail = (const uint8_t *)(data + nblocks*16);
 
   uint64_t k1 = 0;
   uint64_t k2 = 0;
 
-  switch(len & 15)
-    {
-    case 15: k2 ^= uint64_t(tail[14]) << 48;
-    case 14: k2 ^= uint64_t(tail[13]) << 40;
-    case 13: k2 ^= uint64_t(tail[12]) << 32;
-    case 12: k2 ^= uint64_t(tail[11]) << 24;
-    case 11: k2 ^= uint64_t(tail[10]) << 16;
-    case 10: k2 ^= uint64_t(tail[ 9]) << 8;
-    case  9: k2 ^= uint64_t(tail[ 8]) << 0;
+  switch(len & 15) {
+  case 15: k2 ^= uint64_t(tail[14]) << 48;
+  case 14: k2 ^= uint64_t(tail[13]) << 40;
+  case 13: k2 ^= uint64_t(tail[12]) << 32;
+  case 12: k2 ^= uint64_t(tail[11]) << 24;
+  case 11: k2 ^= uint64_t(tail[10]) << 16;
+  case 10: k2 ^= uint64_t(tail[ 9]) << 8;
+  case  9: k2 ^= uint64_t(tail[ 8]) << 0;
 
-    case  8: k1 ^= uint64_t(tail[ 7]) << 56;
-    case  7: k1 ^= uint64_t(tail[ 6]) << 48;
-    case  6: k1 ^= uint64_t(tail[ 5]) << 40;
-    case  5: k1 ^= uint64_t(tail[ 4]) << 32;
-    case  4: k1 ^= uint64_t(tail[ 3]) << 24;
-    case  3: k1 ^= uint64_t(tail[ 2]) << 16;
-    case  2: k1 ^= uint64_t(tail[ 1]) << 8;
-    case  1: k1 ^= uint64_t(tail[ 0]) << 0;
-      bmix64(h1,h2,k1,k2,c1,c2);
-    };
+  case  8: k1 ^= uint64_t(tail[ 7]) << 56;
+  case  7: k1 ^= uint64_t(tail[ 6]) << 48;
+  case  6: k1 ^= uint64_t(tail[ 5]) << 40;
+  case  5: k1 ^= uint64_t(tail[ 4]) << 32;
+  case  4: k1 ^= uint64_t(tail[ 3]) << 24;
+  case  3: k1 ^= uint64_t(tail[ 2]) << 16;
+  case  2: k1 ^= uint64_t(tail[ 1]) << 8;
+  case  1: k1 ^= uint64_t(tail[ 0]) << 0;
+    bmix64(h1,h2,k1,k2,c1,c2);
+  };
 
   //----------
   // finalization
@@ -170,32 +164,30 @@ void MurmurHash3_x64_128 ( const void * key, const int len, const uint32_t seed,
   h1 += h2;
   h2 += h1;
 
-  ((uint64_t*)out)[0] = h1;
-  ((uint64_t*)out)[1] = h2;
+  ((uint64_t *)out)[0] = h1;
+  ((uint64_t *)out)[1] = h2;
 }
 
 //-----------------------------------------------------------------------------
-// If we need a smaller hash value, it's faster to just use a portion of the 
+// If we need a smaller hash value, it's faster to just use a portion of the
 // 128-bit hash
 
-void MurmurHash3_x64_32 ( const void * key, int len, uint32_t seed, void * out )
-{
+void MurmurHash3_x64_32 ( const void *key, int len, uint32_t seed, void *out ) {
   uint32_t temp[4];
 
   MurmurHash3_x64_128(key,len,seed,temp);
 
-  *(uint32_t*)out = temp[0];
+  *(uint32_t *)out = temp[0];
 }
 
 //----------
 
-void MurmurHash3_x64_64 ( const void * key, int len, uint32_t seed, void * out )
-{
+void MurmurHash3_x64_64 ( const void *key, int len, uint32_t seed, void *out ) {
   uint64_t temp[2];
 
   MurmurHash3_x64_128(key,len,seed,temp);
 
-  *(uint64_t*)out = temp[0];
-} 
+  *(uint64_t *)out = temp[0];
+}
 
 //-----------------------------------------------------------------------------
--- src/weights.h
+++ src/weights.h
@@ -9,49 +9,46 @@
 
 using WeightMap = std::unordered_map<int, std::vector<double>>;
 
-std::vector<double> calc_eff_lens(const std::vector<int>& lengths, double mean)
-{
-    // for now do the total naive thing and subtract mean frag length
-    std::vector<double> eff_lens;
-    eff_lens.reserve(lengths.size());
+std::vector<double> calc_eff_lens(const std::vector<int>& lengths, double mean) {
+  // for now do the total naive thing and subtract mean frag length
+  std::vector<double> eff_lens;
+  eff_lens.reserve(lengths.size());
 
-    for (auto& cur_len: lengths)
-    {
-        eff_lens.push_back( static_cast<double>(cur_len) - mean + 1.0 );
-    }
+  for (auto& cur_len: lengths) {
+    eff_lens.push_back( static_cast<double>(cur_len) - mean + 1.0 );
+  }
 
-    return eff_lens;
+  return eff_lens;
 }
 
 
 WeightMap calc_weights(
-        const std::vector<int>& counts,
-        const EcMap& ecmap,
-        const std::vector<double>& eff_lens)
-{
+  const std::vector<int>& counts,
+  const EcMap& ecmap,
+  const std::vector<double>& eff_lens) {
 
-    // TODO: throw some assertions in here to make sure the length of counts
-    // and ec map are correct... as well as eff_lens size is reasonable
+  // TODO: throw some assertions in here to make sure the length of counts
+  // and ec map are correct... as well as eff_lens size is reasonable
 
-    // weights are stored _exactly_ in the same orientation as the ec map
-    WeightMap weights;
+  // weights are stored _exactly_ in the same orientation as the ec map
+  WeightMap weights;
 
-    for (auto& kv : ecmap) {
+  for (auto& kv : ecmap) {
 
-			//std::cout << kv.first;
-        std::vector<double> trans_weights;
-        trans_weights.reserve(kv.second.size());
+    //std::cout << kv.first;
+    std::vector<double> trans_weights;
+    trans_weights.reserve(kv.second.size());
 
-        for (auto& trans_id : kv.second) {
-            trans_weights.push_back( static_cast<double>(counts[kv.first]) /
-                    eff_lens[trans_id] );
-        }
-
-        weights.insert( {kv.first, trans_weights} );
+    for (auto& trans_id : kv.second) {
+      trans_weights.push_back( static_cast<double>(counts[kv.first]) /
+                               eff_lens[trans_id] );
     }
 
+    weights.insert( {kv.first, trans_weights} );
+  }
 
-    return weights;
+
+  return weights;
 }
 
 #endif // KALLISTO_WEIGHTS_H
