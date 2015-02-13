3310ec846077d918c4c6e7c7a27f2a4cf4db6da7
Harold Pimentel <haroldpimentel@gmail.com>
2015-02-12 21:09:15 -0800

    allow Multinomial::sample() to take nsamp. return counts by ref

--- src/Multinomial.hpp
+++ src/Multinomial.hpp
@@ -1,6 +1,7 @@
 #ifndef KALLISTO_MULTINOMIAL_H
 #define KALLISTO_MULTINOMIAL_H
 
+#include <exception>
 #include <random>
 
 class Multinomial {
@@ -8,25 +9,49 @@ class Multinomial {
         Multinomial(const std::vector<int>& counts, size_t seed = 42) :
             counts_(counts),
             gen_(seed),
-            n_(0),
-            dd_(counts_.begin(), counts_.end())
+            dd_(counts_.begin(), counts_.end()),
+            n_(0)
         {
             for (auto c : counts_) {
                 n_ += c;
             }
         }
 
-        std::vector<int> sample() {
+        /**
+         * Generate a sample from the multinomial distribution
+         *
+         * Note that calling this function with nsamp != n() will not result in
+         * a "proper" multinomial, though, it might be useful as long as it is
+         * understood that only samples that have the same nsamp are
+         * comparable. Call sample() for a standard multinomial.
+         *
+         * @param nsamp the number of samples. default == -1, which means it
+         * will default to n_
+         * @return a vector of counts
+         *
+         */
+        std::vector<int> sample(int nsamp) {
+            if (nsamp < 1) {
+                throw std::domain_error("nsamp must be -1 or >=1");
+            }
+
             std::vector<int> samp(counts_.size(), 0.0);
-            for (auto i = 0; i < n_; ++i) {
+            for (auto i = 0; i < nsamp; ++i) {
                 ++samp[dd_(gen_)];
             }
 
             return samp;
         }
 
+        /**
+         * @return a vector of ints with nsamp == n()
+         */
+        std::vector<int> sample() {
+            return sample(n_);
+        }
+
         int n() const { return n_; }
-        std::vector<int> const counts() { return counts_; }
+        const std::vector<int>& counts() { return counts_; }
 
     private:
         const std::vector<int>& counts_;
