c4fad15387d556b9d58d72f25f5d5149977a2bec
Harold Pimentel <haroldpimentel@gmail.com>
2015-04-09 11:39:41 -0700

    tmp fix for HDF5 target_id. also add assertions

--- src/h5utils.cpp
+++ src/h5utils.cpp
@@ -18,17 +18,20 @@ const int* vec_to_ptr(const std::vector<int>& v) {
 }
 
 hid_t get_datatype_id(const std::vector<std::string>& v) {
-  int maxlength = 0;
-  for (auto x : v) {
-    if (x.size() > maxlength) {
-      maxlength = x.size();
+  size_t max_len = 0;
+  for (auto& x : v) {
+    if (x.size() > max_len) {
+      max_len = x.size();
     }
   }
-  
+
+  max_len += 1;
   hid_t datatype_id = H5Tcopy (H5T_C_S1);
-  //H5Tset_size (datatype_id, H5T_VARIABLE);
-  H5Tset_size (datatype_id, maxlength);
-  v.size(); // shutup, compiler
+  herr_t status = H5Tset_size (datatype_id, H5T_VARIABLE);
+  assert( status >= 0 );
+  /* herr_t status = H5Tset_size (datatype_id, max_len); */
+  H5Tset_strpad(datatype_id, H5T_STR_NULLTERM);
+
   return datatype_id;
 }
 
