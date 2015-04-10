0500dcc368aef20856245eba62b5a3663462e76d
Harold Pimentel <haroldpimentel@gmail.com>
2015-04-10 10:38:58 -0700

    fix HDF5 output to write to contiguous blocks of memory

--- src/h5utils.cpp
+++ src/h5utils.cpp
@@ -1,12 +1,24 @@
 #include "h5utils.h"
 
-const char** vec_to_ptr(const std::vector<std::string>& v) {
-  const char** ret;
-  ret = new const char*[v.size()];
-  for (size_t i = 0; i < v.size(); ++i) {
-    ret[i] = v[i].c_str();
+// allocate a contiguous block of memory, dependent on the largest string
+char* vec_to_ptr(const std::vector<std::string>& v) {
+  size_t max_len = 0;
+  for (auto& x : v) {
+    if (x.size() > max_len) {
+      max_len = x.size();
+    }
   }
-  return ret;
+  max_len += 1;
+
+  // allocate a contiguous block of memory
+  char *pool = new char[max_len * v.size()];
+  char *ptr = pool;
+
+  for (size_t i = 0; i < v.size(); ++i, ptr += max_len) {
+    strcpy(ptr, v[i].c_str());
+  }
+
+  return pool;
 }
 
 const double* vec_to_ptr(const std::vector<double>& v) {
@@ -24,13 +36,13 @@ hid_t get_datatype_id(const std::vector<std::string>& v) {
       max_len = x.size();
     }
   }
-
   max_len += 1;
+
   hid_t datatype_id = H5Tcopy (H5T_C_S1);
-  herr_t status = H5Tset_size (datatype_id, H5T_VARIABLE);
+  /* herr_t status = H5Tset_size (datatype_id, H5T_VARIABLE); */
+  /* assert( status >= 0 ); */
+  herr_t status = H5Tset_size (datatype_id, max_len);
   assert( status >= 0 );
-  /* herr_t status = H5Tset_size (datatype_id, max_len); */
-  H5Tset_strpad(datatype_id, H5T_STR_NULLTERM);
 
   return datatype_id;
 }
