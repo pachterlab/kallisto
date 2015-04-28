b87614a256895f3595670a61cd0e86d7d6001557
Harold Pimentel <haroldpimentel@gmail.com>
2015-04-28 10:18:54 -0700

    h5 read vectors

--- src/h5utils.cpp
+++ src/h5utils.cpp
@@ -54,3 +54,82 @@ hid_t get_datatype_id(const std::vector<int>& v) {
   v.size(); // shutup, compiler
   return H5T_NATIVE_INT;
 }
+
+void read_vector(
+    hid_t dataset_id,
+    hid_t datatype_id,
+    hid_t dataspace_id,
+    std::vector<std::string>& out) {
+
+  hsize_t size;
+  hsize_t dims[1];
+
+  H5Sget_simple_extent_dims(dataspace_id, dims, NULL);
+
+  size = H5Tget_size(datatype_id);
+
+  char *pool = new char[ size * dims[0] ];
+  char *ptr = pool;
+  char *buf = new char[size];
+
+  H5Dread(dataset_id, datatype_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, pool);
+  out.reserve(dims[0]);
+
+  for (size_t i = 0; i < dims[0]; ++i, ptr += size) {
+    memcpy(buf, ptr, size);
+    out.push_back( buf );
+  }
+
+  delete pool;
+  delete buf;
+}
+
+void read_vector(
+    hid_t dataset_id,
+    hid_t datatype_id,
+    hid_t dataspace_id,
+    std::vector<int>& out) {
+
+  hsize_t size;
+  hsize_t dims[1];
+
+  H5Sget_simple_extent_dims(dataspace_id, dims, NULL);
+
+  size = H5Tget_size(datatype_id);
+
+  int *pool = new int[ dims[0] ];
+
+  H5Dread(dataset_id, datatype_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, pool);
+  out.reserve(dims[0]);
+
+  for (size_t i = 0; i < dims[0]; ++i) {
+    out.push_back( pool[i] );
+  }
+
+  delete pool;
+}
+
+void read_vector(
+    hid_t dataset_id,
+    hid_t datatype_id,
+    hid_t dataspace_id,
+    std::vector<double>& out) {
+
+  hsize_t size;
+  hsize_t dims[1];
+
+  H5Sget_simple_extent_dims(dataspace_id, dims, NULL);
+
+  size = H5Tget_size(datatype_id);
+
+  double *pool = new double[ dims[0] ];
+
+  H5Dread(dataset_id, datatype_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, pool);
+  out.reserve(dims[0]);
+
+  for (size_t i = 0; i < dims[0]; ++i) {
+    out.push_back( pool[i] );
+  }
+
+  delete pool;
+}
