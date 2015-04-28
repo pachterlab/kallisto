8cfed9f75809d3f5e44da1e4dd7931ac0db254e6
Harold Pimentel <haroldpimentel@gmail.com>
2015-04-28 15:25:47 -0700

    main EM plaintext writer working

--- src/PlaintextWriter.h
+++ src/PlaintextWriter.h
@@ -1,6 +1,7 @@
 #ifndef KALLISTO_PLAINTEXT_WRITER_H
 #define KALLISTO_PLAINTEXT_WRITER_H
 
+#include <assert.h>
 #include <cstdlib>
 
 #include <iostream>
@@ -10,6 +11,7 @@
 
 void plaintext_writer(
     const std::string& out_name,
+    const std::vector<std::string>& targ_ids,
     const std::vector<double>& alpha,
     const std::vector<double>& eff_lens,
     const std::vector<int>& lens
