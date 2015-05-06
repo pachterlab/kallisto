4a2d3bf25693658e6e067cb444d1c069a7afa945
Harold Pimentel <haroldpimentel@gmail.com>
2015-05-05 22:04:22 -0400

    add license and README to release

--- gen_release.sh
+++ gen_release.sh
@@ -6,10 +6,15 @@ if [ -d "$RELEASE" ]; then
 fi
 
 mkdir $RELEASE
+
 cd $RELEASE
 cmake ..
 make
 mkdir kallisto
+
 cp src/kallisto kallisto
+cp ../license.txt kallisto
+cp ../README.md kallisto
+
 tar -cvvf kallisto.tar kallisto
 gzip kallisto.tar
