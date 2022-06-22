5429bfd57cf7b56bcd9bcfa8adb19d000c4973a1
Kristján Eldjárn <kristjan@eldjarn.net>
2022-06-22 15:16:50 -0700

    reference in sort

--- src/MinCollector.cpp
+++ src/MinCollector.cpp
@@ -151,7 +151,7 @@ std::vector<int> MinCollector::intersectECs(std::vector<std::pair<const_UnitigMa
   if (v.empty()) {
     return {};
   }
-  sort(v.begin(), v.end(), [&](std::pair<const_UnitigMap<Node>&, int> a, std::pair<const_UnitigMap<Node>&, int> b)
+  sort(v.begin(), v.end(), [&](std::pair<const_UnitigMap<Node>, int>& a, std::pair<const_UnitigMap<Node>, int>& b)
        {
          if (a.first.isSameReferenceUnitig(b.first) &&
              a.first.getData()->ec[a.first.dist] == b.first.getData()->ec[b.first.dist]) {
