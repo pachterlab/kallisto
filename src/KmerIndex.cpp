3244bd24b3a74539864995449e5442aea8f4aac4
Kristján Eldjárn <kristjan@eldjarn.net>
2022-09-06 16:01:09 -0700

    Attempt to replicate OG kallisto sense/missense behaviour

--- src/KmerIndex.cpp
+++ src/KmerIndex.cpp
@@ -288,6 +288,9 @@ void KmerIndex::BuildEquivalenceClasses(const ProgramOptions& opt, const std::st
         continue;
       }
 
+      Kmer head(seq.substr(proc, k).c_str());
+      bool fwd = (head == head.rep());
+
       proc += um.len;
       const Node* n = um.getData();
       if (trinfos[n->id].size() > EC_THRESHOLD) {
@@ -309,7 +312,7 @@ void KmerIndex::BuildEquivalenceClasses(const ProgramOptions& opt, const std::st
       TRInfo tr;
 
       tr.trid = j;
-      tr.pos = proc | (um.strand ? sense : missense);
+      tr.pos = proc | (um.strand == fwd ? sense : missense);
       tr.start = um.dist;
       tr.stop  = um.dist + um.len;
 
