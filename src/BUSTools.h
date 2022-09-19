4eb3dd95595ad5df89fdea8e8a31a97db6a038c5
Delaney Sullivan <delaneyk.sullivan@gmail.com>
2022-09-19 02:20:46 -0700

    BUSTools.h: Change writeBUSData to return size_t

--- src/BUSTools.h
+++ src/BUSTools.h
@@ -7,7 +7,7 @@
 
 const uint32_t BUSFORMAT_FAKE_BARCODE_LEN = 16;
 
-void writeBUSData(std::ofstream &out, const std::vector<BUSData> &bv);
+size_t writeBUSData(std::ofstream &out, const std::vector<BUSData> &bv);
 void writeBUSHeader(std::ofstream &out, int bclen, int umilen);
 void writeBUSMatrix(const std::string &filename,
                     std::vector<std::vector<std::pair<int,int>>> &data, int cols);
