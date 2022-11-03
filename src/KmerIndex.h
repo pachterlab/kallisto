1aaaf952ef0b22c80ec103c66447fc0507bbf794
Delaney Sullivan <delaneyk.sullivan@gmail.com>
2022-11-03 06:02:20 -0700

    KmerIndex.h: Fix const and typedef KmerSet

--- src/KmerIndex.h
+++ src/KmerIndex.h
@@ -63,6 +63,7 @@ struct RoaringHasher {
   }
 };
 typedef robin_hood::unordered_flat_map<Roaring, int32_t, RoaringHasher> EcMapInv;
+typedef robin_hood::unordered_set<Kmer, KmerHash> KmerSet;
 
 struct KmerEntry {
   int32_t contig; // id of contig
@@ -119,7 +120,7 @@ struct KmerIndex {
 //  bool matchEnd(const char *s, int l, std::vector<std::pair<int, int>>& v, int p) const;
   int mapPair(const char *s1, int l1, const char *s2, int l2) const;
   Roaring intersect(const Roaring& ec, const Roaring& v) const;
-  bool existsInOfflist(const Kmer& km);
+  bool existsInOfflist(const Kmer& km) const;
 
   void BuildTranscripts(const ProgramOptions& opt, std::ofstream& out);
   void BuildDeBruijnGraph(const ProgramOptions& opt, const std::string& tmp_file, std::ofstream& out);
