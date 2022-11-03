343841514a00814338228e6e388c1ed84e6711eb
Delaney Sullivan <delaneyk.sullivan@gmail.com>
2022-11-03 05:45:29 -0700

    KmerIndex.h: add existsInOfflist method

--- src/KmerIndex.h
+++ src/KmerIndex.h
@@ -119,6 +119,7 @@ struct KmerIndex {
 //  bool matchEnd(const char *s, int l, std::vector<std::pair<int, int>>& v, int p) const;
   int mapPair(const char *s1, int l1, const char *s2, int l2) const;
   Roaring intersect(const Roaring& ec, const Roaring& v) const;
+  bool existsInOfflist(const Kmer& km);
 
   void BuildTranscripts(const ProgramOptions& opt, std::ofstream& out);
   void BuildDeBruijnGraph(const ProgramOptions& opt, const std::string& tmp_file, std::ofstream& out);
