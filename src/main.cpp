0aec9500cb683cc05a697221907469e8af0c9092
Rebekah Loving <bekahloving@gmail.com>
2024-05-13 19:43:46 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2485,7 +2485,7 @@ int main(int argc, char *argv[]) {
               flensout_f << " ";
             }
             if (fld_c[i] > 0.5) {
-	      flensout_f << std::fabs((double)fld[i] / (double)fld_c[i] - index.k);//index.target_lens_[i] - (double)fld[i] / (double)fld_c[i] - k); // take mean of recorded uniquely aligning read lengths 
+	      flensout_f << std::fabs(index.target_lens_[i] - (double)fld[i] / (double)fld_c[i] - index.k);//index.target_lens_[i] - (double)fld[i] / (double)fld_c[i] - k); // take mean of recorded uniquely aligning read lengths 
  	    } else {
 	      flensout_f << std::fabs(index.target_lens_[i] - index.k); //index.target_lens_[i] - 31); 
 	    }
